3e1539ac8e6e7d9c4f0beffa2730107bc28317d0
durganp1 <durganp1@gmail.com>
2020-08-28 15:46:43 -0500

    add const and for each

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+const profileDataArgs = process.argv.slice(2);
+
+
+
+const printProfileData = profileDataArr => {
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
