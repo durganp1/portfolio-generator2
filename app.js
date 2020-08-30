2bc1b80b604fa8e9227581630677ab5bdfb9c814
durganp1 <durganp1@gmail.com>
2020-08-30 12:44:42 -0500

    mvp

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const generatePage  = require('./src/page-template');
 const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site');
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
 // const [name, github] = profileDataArgs;
 
@@ -137,9 +138,18 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-    fs.writeFile('./index.html', pageHTML, err => {
-        if (err) throw new Error(err);
-        
-    });
+    return generatePage(portfolioData);
+})
+.then(pageHTML => {
+    return writeFile(pageHTML);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+})
+.then(copyFileResponse => {
+    console.log(copyFileResponse);
+})
+.catch(err => {
+    console.log(err);
 });
